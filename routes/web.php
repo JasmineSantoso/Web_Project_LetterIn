dafddc61045e9a9a7f18d56a5ba1353047249e30
kadiva12 <kadivanurhidayah10@gmail.com>
2026-05-07 10:44:52 +0800

    push

--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,18 @@ Route::post('/signin', [AuthController::class, 'processSignin']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // Book Routes
-Route::get('/browse', [BookController.class, 'browse'])->name('browse');
-Route::get('/book/{id}', [BookController.class, 'details'])->name('book.details');
-Route::get('/search', [BookController.class, 'search'])->name('search');
-Route::get('/book/{id}/review', [BookController.class, 'addReview'])->name('book.review');
+Route::get('/browse', [BookController::class, 'browse'])->name('browse');
+Route::get('/book/{id}', [BookController::class, 'details'])->name('book.details');
+Route::get('/search', [BookController::class, 'search'])->name('search');
+Route::get('/book/{id}/review', [BookController::class, 'addReview'])->name('book.review');
 
 // Profile & Settings
 Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
-    Route::get('/settings', [ProfileController.class, 'settings'])->name('settings');
-    Route::get('/notifications', [SocialController.class, 'notifications'])->name('notifications');
-    Route::get('/bookmates', [SocialController.class, 'bookmates'])->name('bookmates');
+    Route::get('/settings', [ProfileController::class, 'settings'])->name('settings');
+    Route::get('/notifications', [SocialController::class, 'notifications'])->name('notifications');
+    Route::get('/bookmates', [SocialController::class, 'bookmates'])->name('bookmates');
 });
 
 // Public Social
-Route::get('/profile/{id}', [ProfileController.class, 'friendsProfile'])->name('profile.friend');
+Route::get('/profile/{id}', [ProfileController::class, 'friendsProfile'])->name('profile.friend');
