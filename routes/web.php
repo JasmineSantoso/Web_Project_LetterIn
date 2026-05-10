b1be46ec157b3e86c2726de32f8d1297f02929cc
samarawards <samarawardasadiya@gmail.com>
2026-05-10 14:52:13 +0800

    perbaiki unsigned home

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SocialController;
 
 Route::get('/', function () {
+    if (Auth::check()) {
+        return view('home_signed');
+    }
     return view('welcome');
 });
 
